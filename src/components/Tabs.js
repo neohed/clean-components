9f37277fc4a97048b1f5dccf8c754d97ace23759
neohed <35173474+neohed@users.noreply.github.com>
2020-01-23 23:25:15 +0000

    Update Tabs.js

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -3,7 +3,6 @@ import './Tabs.css'
 
 const Tabs = ({children}) => {
     const [activeTab, setActiveTab] = useState(children[0].props.label);
-    const onClickTabItem = (tab) => setActiveTab(tab);
 
     return (
         <div className="tabs">
@@ -12,12 +11,12 @@ const Tabs = ({children}) => {
                     const { label } = props;
 
                     return (
-                        <Tab
-                            activeTab={activeTab}
-                            key={label}
-                            label={label}
-                            onClick={onClickTabItem}
-                        />
+                        <li
+                            className={`tab-list-item${activeTab === label ? ' tab-list-active' : ''}`}
+                            onClick={() => setActiveTab(label)}
+                        >
+                            {label}
+                        </li>
                     );
                 })}
             </ol>
@@ -33,14 +32,5 @@ const Tabs = ({children}) => {
         </div>
     )
 };
-
-const Tab = ({onClick, activeTab, label}) => (
-    <li
-        className={`tab-list-item${activeTab === label ? ' tab-list-active' : ''}`}
-        onClick={() => onClick(label)}
-    >
-        {label}
-    </li>
-);
 
 export default Tabs;
