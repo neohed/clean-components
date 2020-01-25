392ce194240a8de77cd61306299f7a7baf06ad9a
neohed <35173474+neohed@users.noreply.github.com>
2020-01-25 14:08:24 +0000

    Update CleanButton.js

--- src/components/CleanButton.js
+++ src/components/CleanButton.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './cleanButton.css'
 
-const CleanButton = ({text, clickHandler}) => (
+const CleanButton = ({text, id, clickHandler}) => (
     <button
+        value={id}
         className={'clean-button'}
-        onClick={clickHandler}
+        onClick={() => clickHandler(id)}
     >
         {
             text
