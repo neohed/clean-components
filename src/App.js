58db64542d65e45406e4c658b0ea4d0723e5bf56
neohed <35173474+neohed@users.noreply.github.com>
2020-01-28 22:19:39 +0000

    Added Checkbox component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Tabs from './components/Tabs'
-import './App.css';
 import CleanButton from "./components/CleanButton";
+import Checkbox from "./components/Checkbox";
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Tabs>
-        <div label="Gator">
-          See ya later, <em>Alligator</em>!
+    const [isChecked, setIsChecked] = useState(false);
+
+    return (
+        <div className="App">
+            <Tabs>
+                <div label="Gator">
+                    See ya later, <em>Alligator</em>!
+                </div>
+                <div label="Croc">
+                    After 'while, <em>Crocodile</em>!
+                </div>
+                <div label="Sarcosuchus">
+                    Nothing to see here, this tab is <em>extinct</em>!
+                </div>
+            </Tabs>
+            <br/>
+            <hr/>
+            <Checkbox
+                id={'chk1'}
+                isChecked={isChecked}
+                label={'Click me!'}
+                changeHandler={(ctrl, checked) => setIsChecked(checked)}
+            />
+            <hr/>
+            <br/>
+            <CleanButton
+                text={'Clicked Button!'}
+                clickHandler={txt => console.log(txt)}
+            />
         </div>
-        <div label="Croc">
-          After 'while, <em>Crocodile</em>!
-        </div>
-        <div label="Sarcosuchus">
-          Nothing to see here, this tab is <em>extinct</em>!
-        </div>
-      </Tabs>
-      <br/>
-      <hr/>
-      <br/>
-      <CleanButton
-          text={'Clicked Button!'}
-          clickHandler={txt => console.log(txt)}
-      />
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/Checkbox.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Checkbox = ({isChecked, id, label, changeHandler}) => (
+    [
+        <input
+            type="checkbox"
+            key={id + '_chk'}
+            id={id}
+            name={id}
+            checked={isChecked}
+            onChange={({target}) => changeHandler(target.value, !isChecked)}
+        />,
+        <label
+            key={id + '_lbl'}
+            htmlFor={id}
+        >
+            {
+                label
+            }
+        </label>
+    ]
+);
+
+export default Checkbox;
