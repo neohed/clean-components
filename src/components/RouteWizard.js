0c29eb36f87b2dc946abceffa8d19be9b2261c04
neohed <35173474+neohed@users.noreply.github.com>
2021-01-14 12:58:54 +0000

    Add RouteWizard

---
+++ src/components/RouteWizard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './route-wizard.css';
+
+const RouteWizard = () => {
+    return (
+        <div
+            className='route-wizard'
+        >
+
+        </div>
+    );
+};
+
+export default RouteWizard;
