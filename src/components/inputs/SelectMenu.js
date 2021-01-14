203cf33e9ce08d5daf685e495e2f0cb989a89cb8
neohed <35173474+neohed@users.noreply.github.com>
2021-01-14 12:09:27 +0000

    Select menu

---
+++ src/components/inputs/SelectMenu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './select-menu.css';
+
+const SelectMenu = () => {
+    return (
+        <div
+            className='select-menu'
+        >
+
+        </div>
+    );
+};
+
+export default SelectMenu;
