097b102f2d9592fe8f9329e4e26acc6ba1438e58
neohed <35173474+neohed@users.noreply.github.com>
2022-07-19 16:22:28 +0100

    re-order links

--- src/components/RouterMenu.js
+++ src/components/RouterMenu.js
@@ -12,22 +12,16 @@ function RouterMenu() {
                     <Link to="tabs">Tabs</Link>
                 </li>
                 <li>
-                    <Link to="form">Form</Link>
-                </li>
-                <li>
-                    <Link to="scroller">Scroll Over</Link>
-                </li>
-                <li>
                     <Link to="scroll-parallax">Scroll Parallax</Link>
                 </li>
                 <li>
-                    <Link to="mobile-header">Mobile Header</Link>
+                    <Link to="auto-complete">Auto Complete</Link>
                 </li>
                 <li>
-                    <Link to="simple-header">Simple Flexbox Header</Link>
+                    <Link to="timeline">Timeline</Link>
                 </li>
                 <li>
-                    <Link to="flexbox-header">Flexbox Header</Link>
+                    <Link to="emojis">Emojis</Link>
                 </li>
                 <li>
                     <Link to="header-shrink-1">Header Transparent</Link>
@@ -39,6 +33,24 @@ function RouterMenu() {
                     <Link to="header-shrink-3">Header IntersectionObserver</Link>
                 </li>
                 <li>
+                    <Link to="card">Card</Link>
+                </li>
+                <li>
+                    <Link to="form">Form</Link>
+                </li>
+                <li>
+                    <Link to="scroller">Scroll Over</Link>
+                </li>
+                <li>
+                    <Link to="mobile-header">Mobile Header</Link>
+                </li>
+                <li>
+                    <Link to="simple-header">Simple Flexbox Header</Link>
+                </li>
+                <li>
+                    <Link to="flexbox-header">Flexbox Header</Link>
+                </li>
+                <li>
                     <Link to="two-columns">Two Columns</Link>
                 </li>
                 <li>
@@ -48,19 +60,10 @@ function RouterMenu() {
                     <Link to="center">Center</Link>
                 </li>
                 <li>
-                    <Link to="auto-complete">Auto Complete</Link>
-                </li>
-                <li>
-                    <Link to="card">Card</Link>
-                </li>
-                <li>
                     <Link to="layout-header-main">Layout Header Main</Link>
                 </li>
                 <li>
                     <Link to="as-many-as-fit">Layout AsManyAsFit</Link>
-                </li>
-                <li>
-                    <Link to="timeline">Timeline</Link>
                 </li>
                 <li>
                     <Link to="background-offset">Background Offset</Link>
@@ -79,9 +82,6 @@ function RouterMenu() {
                 </li>
                 <li>
                     <Link to="filter-list">Filter List</Link>
-                </li>
-                <li>
-                    <Link to="emojis">Emojis</Link>
                 </li>
                 <li>
                     <Link to="css-filters">CSS Filters</Link>
